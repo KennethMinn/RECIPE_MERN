52f99e9d4b37bab80117e48db7312274efa94a7c
KennethMinn <minthukha2472005@gmail.com>
2024-05-07 13:55:41 +0630

    CRUD services finished

--- server/src/controllers/RecipeController.ts
+++ server/src/controllers/RecipeController.ts
@@ -24,4 +24,28 @@ export const RecipeController = {
     });
     return res.status(200).json(recipe);
   },
+  update: async (req: Request, res: Response) => {
+    const { title, description, ingredients } = req.body;
+    try {
+      const id = req.params.id;
+      const recipe = await Recipe.findById(id);
+      if (!recipe) return;
+      recipe.title = title;
+      recipe.description = description;
+      recipe.ingredients = ingredients;
+      recipe.save();
+      return res.status(200).json(recipe);
+    } catch (error) {
+      return res.status(400).json({ message: "Recipe not found." });
+    }
+  },
+  destroy: async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id;
+      await Recipe.findByIdAndDelete(id);
+      return res.json({ message: "recipe deleted" });
+    } catch (error) {
+      return res.json({ message: "Recipe not found." });
+    }
+  },
 };
--- server/src/routes/recipe_routes.ts
+++ server/src/routes/recipe_routes.ts
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("", RecipeController.index);
 router.get("/:id", RecipeController.show);
 router.post("", recipeValidator, RecipeController.store);
+router.patch("/:id", recipeValidator, RecipeController.update);
+router.delete("/:id", RecipeController.destroy);
 
 export default router;
